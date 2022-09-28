c1215cbb55eb263e336853466d39d44edaddbc9b
kyokio6002 <kyokio0725@gmail.com>
2022-09-28 16:48:06 +0900

    Improve: lint

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,13 @@
 # normal
 alias abc='source ~/Documents/program/command/abc_fish'
 alias prog='cd ~/Documents/program/'
-alias python='python3.9'
-alias python3='python3.9'
 alias vi='nvim'
 alias vim='nvim'
 alias cat='bat --theme=TwoDark'
+
+# python
+alias python='python3.9'
+alias python3='python3.9'
 
 # exa
 alias ls='exa -ghHl --icons --git --sort=ext'
@@ -19,10 +21,10 @@ alias la='exa -ghHl --icons -a --git --sort=ext'
 alias tree='exa -ghHl --tree --icons --git --sort=ext'
 
 # git
-alias gits="git status"
-alias gitd="git diff"
-alias gitl="git log"
+alias gits='git status'
+alias gitd='git diff'
+alias gitl='git log'
 alias gitg="git log --graph --date-order --all --pretty=format:'%h %C(magenta)%ad %C(ul cyan)[%cn]%C(reset) %C(yellow bold)%s %C(red)%w(80)%d' --date=short"
 
 # g++
-alias g++="g++ --std=c++17"
+alias g++='g++ --std=c++17'
