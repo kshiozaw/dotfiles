6c452408a1814c18877c269a840cc7a4ad1a6f35
kyokio6002 <kyokio0725@gmail.com>
2023-09-09 02:34:22 +0900

    Update: 共通化

--- fish/config_work.fish
+++ fish/config_work.fish
@@ -1,5 +1,7 @@
 # kyokio fish
 
+source ./config_common.fish
+
 #######################################
 # PATH
 #######################################
@@ -13,27 +15,8 @@ end
 #######################################
 # alias
 #######################################
-# bat
-alias cat='bat --theme=TwoDark'
-
-# exa
-alias ls='exa -ghHl --icons --git --sort=ext'
-alias la='exa -ghHl --icons --git --sort=ext -a'
-alias tree='exa -ghHl --tree --icons --git --sort=ext'
-
-# git
-alias gits='git status'
-alias gitd='git diff'
-alias gitl='git log'
-alias gitg="git log --graph --date-order --all --pretty=format:'%h %C(magenta)%ad %C(ul cyan)[%cn]%C(reset) %C(yellow bold)%s %C(red)%w(80)%d' --date=short"
-
-# vim
-alias vi='nvim'
-alias vim='nvim'
-alias fvim='vim $(fzf)'
 
 # java
 alias mvn="~/.m2/wrapper/dists/apache-maven-3.8.7-bin/1ktonn2lleg549uah6ngl1r74r/apache-maven-3.8.7/bin/mvn"
 
-# else
-alias myip='curl ifconfig.co/'
+
