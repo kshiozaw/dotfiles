014225d2ffe6a54cee43fd033564afcc7dcc3c23
kyokio6002 <kyokio0725@gmail.com>
2023-09-09 23:43:52 +0900

    update: Using the -u option with the git status

--- fish/config_common.fish
+++ fish/config_common.fish
@@ -15,7 +15,7 @@ alias la='exa -ghHl --icons --git --sort=ext -a'
 alias tree='exa -ghHl --tree --icons --git --sort=ext'
 
 # git
-alias gits='git status'
+alias gits='git status -u'
 alias gitd='git diff'
 alias gitl='git log'
 alias gitg="git log --graph --date-order --all --pretty=format:'%h %C(magenta)%ad %C(ul cyan)[%cn]%C(reset) %C(yellow bold)%s %C(red)%w(80)%d' --date=short"
