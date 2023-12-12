2848838ef99e11c075a606b3da600e6eff16008c
kshiozaw <kshiozaw@yahoo-corp.jp>
2023-12-12 19:18:40 +0900

    update: sdkmanの設定追加

--- fish/config_work.fish
+++ fish/config_work.fish
@@ -11,6 +11,24 @@ if status is-interactive
   set PATH /opt/homebrew/bin $PATH
 end
 
+#######################################
+# SDKMAN
+#######################################
+export JAVA_HOME=$HOME/.sdkman/candidates/java/current
+# set sdkman dir
+if not test -n "$SDKMAN_DIR"
+    set -x SDKMAN_DIR (brew --prefix sdkman-cli)/libexec
+end
+ 
+# sdk command
+function sdk
+    bash -c "source $SDKMAN_DIR/bin/sdkman-init.sh && sdk $argv"
+end
+ 
+# add paths
+for ITEM in $SDKMAN_DIR/candidates/*
+    set -gx PATH $ITEM/current/bin $PATH
+end
 
 #######################################
 # alias
