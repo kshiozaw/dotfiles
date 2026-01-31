ec2e40eeb902dfe27770e26177a059c2bd85c33d
kyokio6002 <kyokio0725@gmail.com>
2026-01-31 22:48:50 +0900

    update: option

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -72,7 +72,14 @@ vim.cmd("colorscheme penny-lane")
 -- ###############################################
 vim.diagnostic.config({
   virtual_text = true,
-  signs = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "✗",
+      [vim.diagnostic.severity.WARN]  = "",
+      [vim.diagnostic.severity.INFO]  = "",
+      [vim.diagnostic.severity.HINT]  = "",
+    }
+  },
   underline = true,
   update_in_insert = true,
   severity_sort = false,
