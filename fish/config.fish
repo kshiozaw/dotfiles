b37d55f777e91bcc849225bfe1af617e22baeb5f
kyokio6002 <kyokio0725@gmail.com>
2026-04-04 19:14:36 +0900

    change: remove fish/config_work

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
-# kyokio fish
-
-source ~/.config/fish/config_common.fish
-
 #######################################
 # PATH
 #######################################
@@ -19,9 +15,24 @@ end
 #######################################
 # alias
 #######################################
-# normal
-alias prog='cd ~/Documents/program/'
-alias g++='g++-14 -std=c++20';
+# eza
+alias ls='eza -ghHl --icons --git --sort=ext'
+alias la='eza -ghHl --icons --git --sort=ext -a'
+alias tree='eza -ghHl --tree --icons --git --sort=ext'
+
+# git
+alias gits='git status -u'
+alias gitd='git diff'
+alias gitl='git log'
+alias gitg="git log --graph --date-order --all --pretty=format:'%h %C(magenta)%ad %C(ul cyan)[%cn]%C(reset) %C(yellow bold)%s %C(red)%w(80)%d' --date=short"
+
+# vim
+alias vi='nvim'
+alias vim='nvim'
+alias fvim='vim $(fzf)'
+
+# else
+alias myip='curl inet-ip.info'
 
 # Added by Antigravity
 fish_add_path /Users/kyokio/.antigravity/antigravity/bin
